2ea938a8498696477a158ff9e488d92a3fe47db7
orthohincom <orthohin.com@gmail.com>
2020-03-13 19:49:50 +0600

    Update orthohin_navbar_info.php

--- orthohin/navbar/acp/orthohin_navbar_info.php
+++ orthohin/navbar/acp/orthohin_navbar_info.php
@@ -12,17 +12,17 @@ namespace orthohin\navbar\acp;
 
 class orthohin_navbar_info
 {
-    public function module()
-    {
-        return array(
-            'filename'  => '\orthohin\navbar\acp\orthohin_navbar_module',
-            'title'     => 'ORTHOHIN_NAVBAR_TITLE',
-            'modes'     => array(
-                'config'        => array('title' => 'ORTHOHIN_NAVBAR_CONFIG', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
-                'menu'          => array('title' => 'ORTHOHIN_NAVBAR_BUTTONS', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
-                'upload'        => array('title' => 'ORTHOHIN_NAVBAR_UPLOAD', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
-                'css'           => array('title' => 'ORTHOHIN_NAVBAR_CSS', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
-            ),
-        );
-    }
+	public function module()
+	{
+		return array(
+			'filename'	=> '\orthohin\navbar\acp\orthohin_navbar_module',
+			'title'		=> 'ORTHOHIN_NAVBAR_TITLE',
+			'modes'		=> array(
+				'config'		=> array('title' => 'ORTHOHIN_NAVBAR_CONFIG', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
+				'menu'			=> array('title' => 'ORTHOHIN_NAVBAR_BUTTONS', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
+				'upload'		=> array('title' => 'ORTHOHIN_NAVBAR_UPLOAD', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
+				'css'			=> array('title' => 'ORTHOHIN_NAVBAR_CSS', 'auth' => 'ext_orthohin/navbar', 'cat' => array('ACP_CAT_DOT_MODS')),
+			),
+		);
+	}
 }
